2caa099530ec7d5f956de76774af4fcb545bd0ef
Sarah Krabacher <37253871+skrabacher@users.noreply.github.com>
2020-03-04 12:15:07 -0800

    added empty component files and folder

---
+++ client/src/components/Courses.js
@@ -0,0 +1,6 @@
+//Stateful Class Component
+// This component provides the "Courses" screen by retrieving the list of courses from the REST API's /api/courses route and rendering a list of courses. Each course needs to link to its respective "Course Detail" screen. This component also renders a link to the "Create Course" screen.
+
+//retrieves data from the REST API
+
+//Pro Tip: Resist the temptation to keep and manage the courses data as global state in the App component. Instead, allow the Courses and CourseDetail components to retrieve their data from the REST API when those components are mounted. Using this approach simplifies the management of the courses data and ensures that the data won't get out of sync with the REST API's persisted data.
