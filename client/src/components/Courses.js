cd29a1d1224d01338be3844190742babf26f07b7
Sarah Krabacher <37253871+skrabacher@users.noreply.github.com>
2020-03-10 21:07:27 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -36,6 +36,7 @@ export default class Courses extends Component {
 
       const courseIndex = this.state.courses;
       const courseLinks = courseIndex.map(course =>
+                                      //need to change link to `/courses/${ course.id }` once detail page is ready
         <div className="grid-33"><Link to="/signin" className="course--module course--link" href="course-detail.html">
               <h4 className="course--label">Course</h4>
               <h3 className="course--title">{ course.title }</h3>
@@ -45,7 +46,7 @@ export default class Courses extends Component {
       return (
         <div className="bounds">
           { courseLinks }
-            {/* LINK TO CREATE COURSE BELOW */}
+            {/* LINK TO CREATE COURSE BELOW need to change to: `/courses/create` once create page is ready*/}
           <div className="grid-33"><Link to="/signup" className="course--module course--add--module" href="create-course.html">
               <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
                   <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
