a8798254ea2f71d23851dceffd366273ec8c302f
Sarah Krabacher <37253871+skrabacher@users.noreply.github.com>
2020-03-12 12:49:46 -0700

    fleshd out form for CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -17,12 +17,12 @@ export default class CreateCourse extends Component {
 
     render(){
         const {
-            title,
-            description,
-            estimatedTime,
-            materialsNeeded,
-            errors
-          } = this.state;
+					title,
+					description,
+					estimatedTime,
+					materialsNeeded,
+					errors
+				} = this.state;
 
         return(
             <div className="bounds course--detail">
@@ -37,35 +37,72 @@ export default class CreateCourse extends Component {
                         </ul>
                     </div>
                     </div>
-                    <form>
-                    <div className="grid-66">
-                        <div className="course--header">
-                        <h4 className="course--label">Course</h4>
-                        <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." defaultValue={""} /></div>
-                        <p>By Joe Smith</p>
-                        </div>
-                        <div className="course--description">
-                        <div><textarea id="description" name="description" className placeholder="Course description..." defaultValue={""} /></div>
-                        </div>
-                    </div>
-                    <div className="grid-25 grid-right">
-                        <div className="course--stats">
-                        <ul className="course--stats--list">
-                            <li className="course--stats--list--item">
-                            <h4>Estimated Time</h4>
-                            <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" defaultValue={""} /></div>
-                            </li>
-                            <li className="course--stats--list--item">
-                            <h4>Materials Needed</h4>
-                            <div><textarea id="materialsNeeded" name="materialsNeeded" className placeholder="List materials..." defaultValue={""} /></div>
-                            </li>
-                        </ul>
-                        </div>
-                    </div>
-                    <div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
-                    </form>
-                </div>
-                </div>
+											<Form 
+												cancel={this.cancel}
+												errors={errors}
+												submit={this.submit}
+												submitButtonText="Sign Up"
+												elements={() => (
+													<React.Fragment>
+														<div className="grid-66">
+																<div className="course--header">
+																<h4 className="course--label">Course</h4>
+																<div><input 
+																	id="title" 
+																	name="title" 
+																	type="text" 
+																	className="input-title course--title--input"
+																	value={title} 
+																	onChange={this.change}  
+																	placeholder="Course title..." 
+																	defaultValue={""} /></div>
+																<p>By Joe Smith</p>
+																</div>
+																<div className="course--description">
+																<div><textarea 
+																	id="description" 
+																	name="description" 
+																	className placeholder="Course description..." 
+																	value={description} 
+																	onChange={this.change}  
+																	defaultValue={""} /></div>
+																</div>
+														</div>
+														<div className="grid-25 grid-right">
+																<div className="course--stats">
+																<ul className="course--stats--list">
+																		<li className="course--stats--list--item">
+																		<h4>Estimated Time</h4>
+																		<div><input 
+																			id="estimatedTime" 
+																			name="estimatedTime" 
+																			type="text" 
+																			className="course--time--input" 
+																			value={estimatedTime} 
+																			onChange={this.change} 
+																			placeholder="Hours" 
+																			defaultValue={""} /></div>
+																		</li>
+																		<li className="course--stats--list--item">
+																		<h4>Materials Needed</h4>
+																		<div><textarea 
+																			id="materialsNeeded" 
+																			name="materialsNeeded" 
+																			className placeholder="List materials..." 
+																			value={materialsNeeded} 
+																			onChange={this.change} 
+																			defaultValue={""} /></div>
+																		</li>
+																</ul>
+																</div>
+														</div>
+														<div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
+													</React.Fragment>
+												)} />//render prop (streamlines DOM formatting)
+											
+                    
+								</div>
+						</div>
 
           
  
